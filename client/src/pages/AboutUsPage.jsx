116e3401dc09cd2bcdd04cbbc2ee92a65098b5df
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-08-02 18:28:26 +0100

    basic set up

---
+++ client/src/pages/AboutUsPage.jsx
@@ -0,0 +1,7 @@
+export default function AboutUsPage() {
+  return (
+    <>
+    About us
+    </>
+  )
+}
---
+++ client/src/pages/ContactUsPage.jsx
@@ -0,0 +1,7 @@
+export default function ContactUsPage() {
+  return (
+    <>
+    Contact us
+    </>
+  )
+}

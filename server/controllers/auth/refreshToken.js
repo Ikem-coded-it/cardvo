cce493756940c05bf191653781dbcf73bd01f1e5
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-12-28 18:06:58 +0100

    fix: token bug

--- server/controllers/auth/refreshToken.js
+++ server/controllers/auth/refreshToken.js
@@ -8,22 +8,29 @@ const { signAccessToken } = require("../../utils/token");
 const refreshUserToken = asyncHandler(async(req, res) => {
   const cookies = req.cookies
 
-  if (!cookies?.jwt) return res.sendStatus(401)
+  if (!cookies?.jwt) {
+    return res.sendStatus(401)
+  }
   const refreshToken = cookies.jwt;
 
   const refreshTokenUser = await dbAsyncQuery(queries.getUserByRefreshToken, [refreshToken]);
 
-  if (refreshTokenUser.rows.length === 0) return res.sendStatus(403);
+  if (refreshTokenUser.rows.length === 0) {
+    return res.sendStatus(403);
+  }
 
   const user = refreshTokenUser.rows[0]
   delete user.refresh_token
   delete user.pass_word
   
   jwt.verify(refreshToken, process.env['JWT_REFRESH_SECRET'], (err, decoded) => {
-    if (err || decoded.email !== user.email) return res.sendStatus(403);
-    const newAccessToken = signAccessToken(user);
-    user.accessToken = newAccessToken;
-    res.json({user: user})
+    if (err || decoded.email !== user.email) {
+      return res.sendStatus(403);
+    } else {
+      const newAccessToken = signAccessToken(user);
+      user.accessToken = newAccessToken;
+      return res.json({user: user})
+    }
   });
 })
 

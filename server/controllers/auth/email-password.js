48a6780da70215d6e02839a9f29e64c1d9ff073d
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2024-01-04 11:47:56 +0100

    testing refresh token in prod

--- server/controllers/auth/email-password.js
+++ server/controllers/auth/email-password.js
@@ -105,7 +105,7 @@ const loginUser = asyncHandler(async(req, res) => {
     {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      // sameSite: process.env['NODE_ENV'] === "development" ? 'Lax' : 'None',
+      sameSite: 'Lax',
       secure: process.env['NODE_ENV'] === "development" ? false : true,
       domain: process.env['NODE_ENV'] === "development" ? "localhost:3000" : process.env['LIVE_DOMAIN'],
       path: "/api/v1"
